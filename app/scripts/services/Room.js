02c1f43a68d7135687bacd3851e132409abaf326
Richard Newman <richard@newmanworks.com>
2016-11-11 10:15:11 -0700

    update Firebase connection

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,7 +1,8 @@
 (function() {
   function Room($firebaseArray) {
-    var firebaseRef = new Firebase('radiant-fire-2157.firebaseio.com');
-    var rooms = $firebaseArray(firebaseRef.child('rooms'));
+    // var firebaseRef = new Firebase('radiant-fire-2157.firebaseio.com');
+    var ref = firebase.database().ref();
+    var rooms = $firebaseArray(ref.child('rooms'));
 
     return {
       all: rooms,
